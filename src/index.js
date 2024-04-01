ccd8373f81f4d943c8e4e83f5030e1888c2b7311
mohamadlaqees <mohamadlk24@gmail.com>
2024-04-01 08:50:04 +0300

    remove file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("myInput");
-const errorText = document.getElementById("errorText");
-
-if (input.value.trim() === "") {
-    errorText.textContent = "Input cannot be left blank.";
-} else {
-    errorText.textContent = "";
-}

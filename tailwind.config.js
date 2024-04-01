0c7951bee0e30df37967c7bb4c5bf4d73ca068a0
mohamadlaqees <mohamadlk24@gmail.com>
2024-04-01 08:47:36 +0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#01314a",
+        text2: "#4b5563",
+        nav: "#065c8b",
+        box: "#005a87",
+        butt: "rgb(232 95 60 / var(--tw-bg-opacity))",
+      },
+      width: {
+      },
+      backgroundImage: {
+        back: "radial-gradient(ellipse at center, #1e5799 0, #013652 63%, #002031 100%)",
+      },
+      content: {
+        empty: "",
+      },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
